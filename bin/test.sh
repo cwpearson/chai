d3e01583c2f08fd2edda58f833c9b576f299d1be
Carl Pearson <pearson@illinois.edu>
2016-12-17 16:05:55 -0600

    Test file prints benchmark

--- bin/test.sh
+++ bin/test.sh
@@ -1,16 +1,16 @@
 set -e
 
-cd "$BENCH_ROOT"/BFS  && ./bfs
-cd "$BENCH_ROOT"/BS   && ./bs
-cd "$BENCH_ROOT"/CEDD && ./cedd
-cd "$BENCH_ROOT"/CEDT && ./cedt
-cd "$BENCH_ROOT"/HSTI && ./hsti
-cd "$BENCH_ROOT"/HSTO && ./hsto
-cd "$BENCH_ROOT"/PAD  && ./pad
-cd "$BENCH_ROOT"/RSCD && ./rscd
-cd "$BENCH_ROOT"/RSCT && ./rsct
-cd "$BENCH_ROOT"/SC   && ./sc
-cd "$BENCH_ROOT"/SSSP && ./sssp
-cd "$BENCH_ROOT"/TQ   && ./tq
-cd "$BENCH_ROOT"/TQH  && ./tqh
-cd "$BENCH_ROOT"/TRNS && ./trns
+cd "$BENCH_ROOT"/BFS  && echo BFS  && ./bfs
+cd "$BENCH_ROOT"/BS   && echo BS   && ./bs
+cd "$BENCH_ROOT"/CEDD && echo CEDD && ./cedd
+cd "$BENCH_ROOT"/CEDT && echo CEDT && ./cedt
+cd "$BENCH_ROOT"/HSTI && echo HSTI && ./hsti
+cd "$BENCH_ROOT"/HSTO && echo HSTO && ./hsto
+cd "$BENCH_ROOT"/PAD  && echo PAD  && ./pad
+cd "$BENCH_ROOT"/RSCD && echo RSCD && ./rscd
+cd "$BENCH_ROOT"/RSCT && echo RSCT && ./rsct
+cd "$BENCH_ROOT"/SC   && echo SC   && ./sc
+cd "$BENCH_ROOT"/SSSP && echo SSSP && ./sssp
+cd "$BENCH_ROOT"/TQ   && echo TQ   && ./tq
+cd "$BENCH_ROOT"/TQH  && echo TQH  && ./tqh
+cd "$BENCH_ROOT"/TRNS && echo TRNS && ./trns
