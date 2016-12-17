f5666bc38b3d893b9ec647b9da5ad06cb068e53f
Carl Pearson <pearson@illinois.edu>
2016-12-17 09:14:43 -0600

    Add test script

---
+++ bin/test.sh
@@ -0,0 +1,16 @@
+set -e
+
+cd "$BENCH_ROOT"/BFS  && ./bfs
+cd "$BENCH_ROOT"/BS   && ./bs
+cd "$BENCH_ROOT"/CEDD && ./cedd
+cd "$BENCH_ROOT"/CEDT && ./cedt
+cd "$BENCH_ROOT"/HSTI && ./hsti
+cd "$BENCH_ROOT"/HSTO && ./hsto
+cd "$BENCH_ROOT"/PAD  && ./pad
+cd "$BENCH_ROOT"/RSCD && ./rscd
+cd "$BENCH_ROOT"/RSCT && ./rsct
+cd "$BENCH_ROOT"/SC   && ./sc
+cd "$BENCH_ROOT"/SSSP && ./sssp
+cd "$BENCH_ROOT"/TQ   && ./tq
+cd "$BENCH_ROOT"/TQH  && ./tqh
+cd "$BENCH_ROOT"/TRNS && ./trns
